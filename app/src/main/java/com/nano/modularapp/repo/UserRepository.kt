e4dbfbecbc9c6a64aaecab1e0dcfbb90644a6d27
neeraj_mac <neeraj1108yadav@gmail.com>
2024-07-23 20:03:47 +0530

    design update

--- app/src/main/java/com/nano/modularapp/repo/UserRepository.kt
+++ app/src/main/java/com/nano/modularapp/repo/UserRepository.kt
@@ -1,7 +1,14 @@
 package com.nano.modularapp.repo
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.nano.modularapp.api.UserService
+import com.nano.modularapp.model.User
 import com.nano.modularapp.model.UserRequest
+import com.nano.modularapp.model.UserResponse
+import com.nano.modularapp.network.NetworkResult
+import org.json.JSONObject
+import retrofit2.Response
 import javax.inject.Inject
 
 /**
@@ -9,11 +16,31 @@ import javax.inject.Inject
  */
 class UserRepository @Inject constructor(private val userService: UserService) : OnBoardRepository {
 
+    private val _networkResultState:MutableLiveData<NetworkResult<UserResponse>> = MutableLiveData()
+    val networkResultState:LiveData<NetworkResult<UserResponse>>
+        get() = _networkResultState
+
     override suspend fun login(userRequest: UserRequest) {
-       userService.login(userRequest)
+       _networkResultState.postValue(NetworkResult.Loading())
+        val response = userService.login(userRequest)
+        handleResponse(response)
     }
 
     override suspend fun signup(userRequest: UserRequest) {
-        userService.signup(userRequest)
+        _networkResultState.postValue(NetworkResult.Loading())
+        val response = userService.signup(userRequest)
+        handleResponse(response)
     }
+
+    private fun handleResponse(response: Response<UserResponse>) {
+        if (response.isSuccessful && response.body() != null) {
+            _networkResultState.postValue(NetworkResult.Success(response.body()))
+        } else if (response.errorBody() != null) {
+             val errorObj = JSONObject(response.errorBody()!!.charStream().readText())
+            _networkResultState.postValue(NetworkResult.Failure(errorObj.optString("message")))
+        } else {
+            _networkResultState.postValue(NetworkResult.Failure("Something went wrong"))
+        }
+    }
+
 }
