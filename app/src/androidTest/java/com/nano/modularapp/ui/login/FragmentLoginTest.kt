95d4f7f696376940923809d09718e259c1534197
neeraj_mac <neeraj1108yadav@gmail.com>
2024-08-30 14:21:50 +0530

    android ui test navigate on button press to different fragment

--- app/src/androidTest/java/com/nano/modularapp/ui/login/FragmentLoginTest.kt
+++ app/src/androidTest/java/com/nano/modularapp/ui/login/FragmentLoginTest.kt
@@ -6,6 +6,7 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.core.app.launchActivity
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -16,6 +17,7 @@ import com.nano.modularapp.HiltTestActivity
 import com.nano.modularapp.MainActivity
 import com.nano.modularapp.R
 import com.nano.modularapp.launchFragmentInHiltContainer
+import com.nano.modularapp.ui.signup.FragmentSignUp
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -46,7 +48,7 @@ class FragmentLoginTest{
     }
 
     @Test
-    fun test_blankField_expectedEmptyField(){
+    fun test_login_fragment_displayed(){
 
         launchFragmentInHiltContainer<FragmentLogin>(){
            /* val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
@@ -59,21 +61,30 @@ class FragmentLoginTest{
         onView(withId(R.id.loginFragmentView)).check(ViewAssertions.matches(isDisplayed()))
     }
 
-    /*@Test
-    fun test_empty_field_expectedTrue(){
+    @Test
+    fun test_navigation_success_from_login_to_signup(){
+        val navController = TestNavHostController(ApplicationProvider.getApplicationContext())
+        InstrumentationRegistry.getInstrumentation().runOnMainSync {
+            navController.setGraph(R.navigation.onboard_graph)  // Replace with your actual nav graph
+        }
+
+        launchFragmentInHiltContainer<FragmentLogin>(){
+             navController.setCurrentDestination(R.id.loginFragment)
+             Navigation.setViewNavController(requireView(), navController)
+        }
+
         //Checking LoginFragment is attached and displayed in screen or not
-        //onView(withId(R.id.loginFragment)).check(ViewAssertions.matches(isDisplayed()))
+        onView(withId(R.id.loginFragmentView)).check(ViewAssertions.matches(isDisplayed()))
 
         //Clicking on Sign Up text to navigate to Sign Up Fragment
-        //onView(withId(R.id.navigateToSignUp)).perform(ViewActions.click())
+        onView(withId(R.id.navigateToSignUp)).perform(ViewActions.click())
+
+        launchFragmentInHiltContainer<FragmentSignUp>(){
+            //navController.setCurrentDestination(R.id.signUpFragment)
+            //Navigation.setViewNavController(requireView(), navController)
+        }
 
         //Checking SignUpFragment is displayed or not
-        //onView(withId(R.id.fragmentSignUp)).check(ViewAssertions.matches(isDisplayed()))
-
-        //Clicking Sign Up Button
-        //onView(withId(R.id.btnSignUp)).perform(ViewActions.click())
-
-        //Checking empty email error message
-        //onView(withId(R.id.tvError)).check(ViewAssertions.matches(withText("Email field is empty")))
-    }*/
+        onView(withId(R.id.signUpFragmentView)).check(ViewAssertions.matches(isDisplayed()))
+    }
 }
